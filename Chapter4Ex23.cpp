e5efb4754ae38fffb9699e8664ac3f325ba6a588
Mark Goldstein <GoldsteinM@vpcc.edu>
2025-03-21 11:45:45 -0400

    new code

--- Chapter4Ex23.cpp
+++ Chapter4Ex23.cpp
@@ -11,80 +11,117 @@ end the program.
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+using namespace std;
+void DisplayMenu();
+int  GetChoice();
+double CalculateAreaOfCircle(double radius);
+double AskForRadius();
+double CalculateAreaOfRectangle(double height = 10.0, double width = 5.0);
+double GetDimension(string text);
+double CalculateAreaOfTriangle(double height = 10, double base = 20.0);
+void ExecuteChoice(int choice);
+
 
 int main()
 {
-	using namespace std;
+ 
 	int choice;
-	double radius, base, height, rheight, width;                          //defining variables
+	 
+	DisplayMenu();
+
+	do {
+		
+		choice = GetChoice();
+		ExecuteChoice(choice);
+		DisplayMenu();
+	} while (choice != 4);
+
+}
 
 
+void DisplayMenu()
+{
 	cout << "Geometry calculator" << endl;                                //menu display
 	cout << "     1. calculate the area of a circle" << endl;
 	cout << "     2. calculate the area of a rectangle" << endl;
 	cout << "     3. calculate the area of a triangle" << endl;
 	cout << "     4. Exit" << endl;
 	cout << "     Enter your choice (1-4)" << endl;
+}
+int GetChoice() {
+	int choice;
 	cin >> choice;
-	if (choice < 1 || choice>4)                                           //input validation: choice
+	while (choice < 1 || choice>4)                                           //input validation: choice
+	{
 		cout << "ERROR! invalid selction. Restart and choose 1-4" << endl;
-
-	switch (choice) {
-	case 1: {                                                             //circle
-		cout << "circle." << endl;
-		cout << "What is the radius: ";
-		cin >> radius;
-		if (radius < 0)                                                   //input validation: radius
-			cout << "Number is negative. Start over." << endl;
-		else {
-			double circle = 3.14159 * pow(radius, 2);
-			cout << "area is " << circle << endl;
-		}
-		break;
+		cin >> choice;
 	}
-	case 2: {                                                             //rectangle
-		cout << "rectangle." << endl;
-		cout << "Enter the height: " << endl;
-		cin >> rheight;
-		if (rheight < 0) {                                                //input validation: rectangle height
-			cout << "Number is negative. Start over." << endl;
-		}
-		else {
-			cout << "Enter the width: " << endl;
-			cin >> width;                                                 //input validation: width
-			if (width < 0) {
-				cout << "Number is negative. Start over." << endl;
-			}
-			else {
-				double rectangle = rheight * width;
-				cout << "area is " << rectangle << endl;
-			}
-		}
-		break;
-	}
-	case 3: {                                                             //triangle
-		cout << "Triangle." << endl;
-		cout << "Enter the height:" << endl;
-		cin >> height;
-		if (height < 0) {                                                 //input validation: height
-			cout << "Number is negative. Start over." << endl;
-		}
-		else {
-			cout << "Enter the base: " << endl;
-			cin >> base;
-			if (base < 0) {                                               //input validation: base
-				cout << "Number is negative. Start over." << endl;
-			}
-			else {
-				double triangle = base * height / 2;
-				cout << "area is " << triangle << endl;
-			}
-		}
-		break;
-
-
-	}
-	case 4:cout << "     Goodbye" << endl;                                //end program
-	}
+	return choice;
 
 }
+
+double GetDimension(string text) {
+	double dimension;
+	cout << text;
+	cin >> dimension;
+	return dimension;
+
+double CalculateAreaOfTriangle()
+{
+	cout << "Triangle." << endl;
+	double height = GetDimension("Enter the height of the triangle");
+	double base = GetDimension("Enter the base of the triangle");
+	double AreaOfTriangle = CalculateAreaofTriangle(height,base);
+
+ 
+	return area = base * height / 2;
+	 
+	}
+}
+
+double CalculateAreaOfRectangle()
+{
+	cout << "Rectangle." << endl;
+	double height = GetDimension("Enter the height of the rectangle");
+	double width = GetDimension("Enter the width of the rectangle");
+	 
+
+
+	return height * width;
+
+
+}
+double CalculateAreaOfCircle(double radius)
+{
+	return pow(radius, 2);
+
+}
+void ExecuteChoice(int choice)
+{
+
+	switch (choice) {
+	case 1:
+		double radius = AskForRadius();
+		DisplayAnswer(CalculateAreaOfCircle(radius));
+		break;
+
+	case 2: {
+		double h = GetDimension("Please enter the height:");//rectangle
+		double w = GetDimension("Please enter the width:");//rectangle
+		DisplayAnswer(CalculateAreaOfRectangle(h, w));
+		break;
+	}
+	case 3: {
+		double h = GetDimension("Please enter the height:");//rectangle
+		double b = GetDimension("Please enter the base:");//rectangle//triangle
+		DisplayAnswer(CalculateAreaOfTriangle(h, b));
+		break;
+
+	case 4:cout << "     Goodbye" << endl;
+	}
+}
+
+
+ 
+
+
